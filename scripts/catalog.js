152238acb598043c9ecf93d9a8654c4b3bc15a42
appucyriac <appu@qburst.com>
2017-12-14 20:26:25 +0530

    choose product and add to cart

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -1,38 +1,47 @@
-let products = [
-					{"id":"1",
-					 "company":"a",
-					 "name":"candle",
-					 "size":"small",
-					 "price":10,
-					 "discount":5
-					},
-					{"id":"2",
-					 "company":"a",
-					 "name":"candle",
-					 "size":"large",
-					 "price":15,
-					 "discount":6
-					},
-					{"id":"3",
-					 "company":"b",
-					 "name":"candle",
-					 "size":"small",
-					 "price":8,
-					 "discount":4
-					},
-					{"id":"1",
-					 "company":"a",
-					 "name":"candle",
-					 "size":"medium",
-					 "price":12,
-					 "discount":5
-					}
+const products = [{
+		"id": "1",
+		"company": "a",
+		"name": "red candle",
+		"type": "candle",
+		"size": "small",
+		"price": 10,
+		"discount": 5
+	}, {
+		"id": "2",
+		"company": "a",
+		"name": "green candle",
+		"type": "candle",
+		"size": "large",
+		"price": 15,
+		"discount": 6
+	}, {
+		"id": "3",
+		"company": "b",
+		"name": "red candle",
+		"type": "candle",
+		"size": "small",
+		"price": 8,
+		"discount": 4
+	}, {
+		"id": "1",
+		"company": "a",
+		"name": "blue candle",
+		"type": "candle",
+		"size": "medium",
+		"price": 12,
+		"discount": 5
+	}
 
-	            ]
+]
+const gst = {
+	"soap": 1,
+	"candle": 2,
+	"toothpaste": 1
+}
 
 let local = new localStorageHandler();
-$(document).ready(() => 
-{
-  local.set("product-catalog",JSON.stringify(products));
+$(document).ready(() => {
+	local.set("product-catalog", JSON.stringify(products));
+	local.set("gst", JSON.stringify(gst));
 
 });
--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -1,63 +1,105 @@
-class localStorageHandler{
-  constructor(){
-      let ls = window.localStorage;
-      this.get = ((key)=>
-      {
-           return ls.getItem(key);
-      });
-      this.set = ((key, val)=> 
-      {
-           ls.setItem(key,val);
-      });
-  }
+class localStorageHandler {
+	constructor() {
+		let ls = window.localStorage;
+		this.get = ((key) => {
+			return ls.getItem(key);
+		});
+		this.set = ((key, val) => {
+			ls.setItem(key, val);
+		});
+	}
 }
 
 let lsh = new localStorageHandler(),
-    shopListArray = [],
-    flag = false;
+	shopListArray = [];
 
-getInput = () =>
-{ 
-  let productName = $("#product-input")[0].value; 
-      productSize = $("#size-input")[0].value,
-      newList = {
-                      name:productName,
-                      size:productSize
-                };
-  storeList(newList);
+getInput = () => {
+	let productType = $("#product-input")[0].value;
+	productSize = $("#size-input")[0].value,
+		newList = {
+			type: productType,
+			size: productSize
+		};
+	storeList(newList);
 }
 
-storeList =(newListItem)=>{
-lsh.set("newShoplist",JSON.stringify(newListItem));
-searchCatalog();
+storeList = (newListItem) => {
+	lsh.set("newShoplist", JSON.stringify(newListItem));
+	searchCatalog();
 }
 
-searchCatalog = () =>
-{
-  $("ul").empty();
-  $(".results__header").show();  
-  shopListArray = JSON.parse(lsh.get("newShoplist"));
-  productCatalog = JSON.parse(lsh.get("product-catalog"));
-  temp1 = _.filter(productCatalog, { 'name': shopListArray.name});
-  temp2 = _.filter(productCatalog, { 'name': shopListArray.name,'size': shopListArray.size});
-  if(temp1.length != 0)
-  {if(temp2.length != 0)
-   {
-   	_.forEach(temp2,function(value){
-   		$("#search-results").append("<li>"  + value.name + "&emsp;&emsp;&emsp;"+ value.price + "&emsp;&emsp;&emsp;" + value.company + "</li>");
-   	});
-   }
-   else
-   {
-   	_.forEach(temp1,function(value){
-   		$("#search-results").append("<li>"  + value.name + "&emsp;&emsp;&emsp;"+ value.price + "&emsp;&emsp;&emsp;" + value.company + "</li>");
-   	});
-   }
-  }
-  else
-  {
-  	$(".results__header").hide(); 
-  	$("#search-results").append("<li>No results found !</li>");
-  }
-}
+searchCatalog = () => {
+	$("ul").empty();
+	$(".results__header").show();
+	shopListArray = JSON.parse(lsh.get("newShoplist"));
+	productCatalog = JSON.parse(lsh.get("product-catalog"));
+	allNamesWithType = _.filter(productCatalog, {
+		'type': shopListArray.type
+	});
+	allNamesWithSize = _.filter(productCatalog, {
+		'type': shopListArray.type,
+		'size': shopListArray.size
+	});
+	if (allNamesWithType.length != 0) {
+		if (allNamesWithSize.length != 0) {
+			found=allNamesWithSize;
+			lsh.set("found",JSON.stringify(found));
+			i=0;
+			_.forEach(found, function(value) {
+				$("#search-results").append("<li class=resultRow"+i+">"+
+					'<input type="checkbox" class="'+value.id+'"/>' + value.name +
+				 "&emsp;&emsp;&emsp;" + value.price + "&emsp;&emsp;&emsp;" + value.company +
+				 '<input type="number" class="temp" min=0>' + "</li>");
+				i=i+1;
+			});
+		} else {
+			if (allNamesWithSize == "") {
+				found=allNamesWithType;
+				lsh.set("found",JSON.stringify(found));
+				_.forEach(found, function(value) {
+					$("#search-results").append("<li>" + value.name + 
+						"&emsp;&emsp;&emsp;" + value.price + "&emsp;&emsp;&emsp;"
+						 + value.company + "</li>");
+				});
+			} else {
+				$(".results__header").hide();
+				$("#search-results").append("<li>No results found !</li>");
+			}
+		}
+	$(".proceedButton").show();
+	} else {
+		$(".results__header").hide();
+		$("#search-results").append("<li>No results found !</li>");
+	}
+	//$('<input type="checkbox" class="resultRow"'+i+'/>').appendTo('li');
 
+}
+addToCart = () =>{
+  foundResults=JSON.parse(lsh.get("found"));
+  cart=[];
+  checkedId=$('input:checked').map(function () { return $(this).attr('class'); }).get();
+  quantity=$('.temp').map(function () { return $(this).val() }).get();
+  _.forEach(checkedId, function(value){
+  cart.push(_.filter(foundResults, {
+		'id': value
+	})); 
+	});
+    i=0;
+  _.forEach(cart,function(value){
+  	value[0]['qty']=quantity[i];
+  	i++;
+  });
+  lsh.set("cart",JSON.stringify(cart)); 
+  $(".results").hide();
+  $(".checkout-box").show();
+  calculateAmount();
+}
+calculateAmount = () =>{
+cart=JSON.parse(lsh.get("cart"));
+_.forEach(cart, function(value){
+   newRow="<div><span>"+ value[0].name+"&emsp;" + "</span>"+  +"<span>"+ value[0].qty+"&emsp;" +
+    "</span>"+"<span>"+ value[0].price + "</span></div>";
+   $(".checkout-box").append(newRow);
+});
+productGst = JSON.parse(lsh.get("gst"));
+}
